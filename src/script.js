51a970667384fe6d9d1fbd44b0f3d270d089cfa9
nk-on <2nkoz@proton.me>
2024-01-18 22:21:45 +0400

    feat:adding sqaures to the board container according to numbers entered by user

--- src/script.js
+++ src/script.js
@@ -5,3 +5,19 @@ const rainbowModeButton = document.querySelector('.option.rainbow-mode');
 const eraserButton = document.querySelector('.option.eraser');
 const clearButton = document.querySelector('.option.clear');
 const drawingBoard = document.querySelector(".board");
+//app should create grid on the board which will be from 1X1 to 64X64
+  //run loop from 1 to loop.value**2 
+  //create squares
+  //insert them to the board
+//it should have grid tracker below the input range
+gridRange.addEventListener("change",()=>{
+    createGrid(gridRange.value**2);
+});
+function createGrid(gridNumber){
+    for(let i = 1; i <= gridNumber;i++){
+        const square = document.createElement("div");
+        square.classList.add("square");
+        drawingBoard.appendChild(square);
+        square.style.flex = `${Math.sqrt(gridNumber)}%`
+    };
+}
