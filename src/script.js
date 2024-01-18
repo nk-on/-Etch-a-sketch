c2d321413477365c1531495415488ef0fcfa1c1c
nk-on <2nkoz@proton.me>
2024-01-18 21:35:18 +0400

    defining variables inside javascript to work with

---
+++ src/script.js
@@ -0,0 +1,7 @@
+const gridRange = document.querySelector("#grid-range");
+const colorPicker = document.querySelector("#board");
+const colorModeButton = document.querySelector('.option.color-mode');
+const rainbowModeButton = document.querySelector('.option.rainbow-mode');
+const eraserButton = document.querySelector('.option.eraser');
+const clearButton = document.querySelector('.option.clear');
+const drawingBoard = document.querySelector(".board");
