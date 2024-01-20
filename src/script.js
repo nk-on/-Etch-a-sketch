28ac2839f2933b5c399d783457b8ed850ee732aa
nk-on <2nkoz@proton.me>
2024-01-20 20:27:39 +0400

    feat:implementing feature which displays random rainbow colors in case of clicking rainbow mode button

--- src/script.js
+++ src/script.js
@@ -1,11 +1,12 @@
 const gridRange = document.querySelector("#grid-range");
 const gridlabel = document.querySelector("#grid-label")
 const colorPicker = document.querySelector("#color-picker");
-const colorModeButton = document.querySelector('.option.color-mode');
-const rainbowModeButton = document.querySelector('.option.rainbow-mode');
+const colorModeButton = document.querySelector('.color-mode');
+const rainbowModeButton = document.querySelector('.rainbow-mode');
 const eraserButton = document.querySelector('.eraser');
 const clearButton = document.querySelector('.clear');
 const drawingBoard = document.querySelector(".board");
+const buttons = document.querySelectorAll("button")
 gridRange.addEventListener("change", () => {
     createGrid(gridRange.value ** 2);
 });
@@ -22,10 +23,25 @@ function createGrid(gridNumber) {
     };
     gridlabel.textContent = `${gridRow}X${gridRow}`;
     const childen = drawingBoard.childNodes;
-    childen.forEach((square) => {
-        square.addEventListener("mouseover", displayColor);
+    buttons.forEach((button)=>{
+        button.addEventListener("click",()=>{
+            if(button.classList.contains("color-mode")){
+                childen.forEach((square) => {
+                    square.addEventListener("mousemove", displayColor);
+                });
+            }else if(button.classList.contains("rainbow-mode")){
+                childen.forEach((square) => {
+                    square.addEventListener("mousemove",displayRainbowColor);
+                });
+            }
+        });
     });
 };
 function displayColor() {
     this.style.background = colorPicker.value;
+};
+function displayRainbowColor(){
+    const rainbowHexCodes = ['#FF0000', '#FFA500', '#FFFF00', '#008000', '#0000FF', '#4B0082', '#EE82EE'];
+    const randomIdx = Math.floor(Math.random()*rainbowHexCodes.length-1);
+    this.style.background = rainbowHexCodes[randomIdx];
 };
